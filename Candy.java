d95a3fea05b8e9e3152c5257d323f3e0924ae7c5
rosalindc2 <rosalindc2@nycstudents.net>
2025-05-05 22:06:11 +0000

    done

---
+++ Candy.java
@@ -0,0 +1,15 @@
+public class Candy {
+    private String flavor; 
+    public Candy(String f) 
+    {
+        flavor = f;
+    }
+    public String getFlavor() 
+    {
+        return flavor;
+    }
+        public String toString()
+    {
+        return flavor;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) 
+    {
+        /*Candy[][] box = new Candy [4][3];
+        box[0][1] = new Candy("lime");
+        box[1][1] = new Candy("orange");
+        box[2][2] = new Candy("cherry");
+        box[3][1] = new Candy("lemon");
+        box[3][2] = new Candy("grape");
+
+        BoxOfCandy b = new BoxOfCandy(box);
+        System.out.println(b); */
+        Candy[][] box = new Candy[3][5]; 
+
+        box[0][0] = new Candy("lime");
+        box[0][1] = new Candy("lime");
+        box[0][3] = new Candy("lemon");
+        box[1][0] = new Candy("orange");
+        box[1][3] = new Candy("lime");
+        box[1][4] = new Candy("lime");
+        box[2][0] = new Candy("cherry");
+        box[2][2] = new Candy("lemon");
+        box[0][4] = new Candy("orange");
+
+        BoxOfCandy b = new BoxOfCandy(box);
+
+        System.out.println(b.removeNextByFlavor("cherry"));
+        System.out.println(b.removeNextByFlavor("lime"));
+        System.out.println(b.removeNextByFlavor("grape"));
+        System.out.println(b);
+
+    }
+}
